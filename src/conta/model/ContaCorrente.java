9a3a4e01f9dad13531c130c595d91df8fbc82c2e
Lucas Alexandrino <lucas.alexcontato@gmail.com>
2024-06-03 11:15:31 -0300

    Update README.MD

--- src/conta/model/ContaCorrente.java
+++ src/conta/model/ContaCorrente.java
@@ -9,7 +9,7 @@ public class ContaCorrente extends Conta {
 	public ContaCorrente(int numero, int agencia, int tipo, String titular, float saldo, float limite) {
 		super(numero, agencia, tipo, titular, saldo);
 		this.limite = limite;
-		
+
 	}
 
 	public float getLimite() {
@@ -38,5 +38,5 @@ public class ContaCorrente extends Conta {
 		super.visualizar();
 		System.out.println("Limite de Crédito: " + this.limite);
 	}
-	
+
 }
